1554d9a1a0b62266ed4089e75d1e864199367d60
d9h20f <>
2018-01-30 18:10:52 +0100

    Dutch translation

---
+++ translations/systemdatascope-nl.ts
@@ -0,0 +1,327 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name>AppStatus</name>
+    <message>
+        <source>&lt;b&gt;Folder with the collectd databases while running:&lt;/b&gt; </source>
+        <translation>&lt;b&gt;Map met collectd-databases tijdens uitvoeren:&lt;/b&gt; </translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Folder with the collectd databases while the daemon is stopped:&lt;/b&gt; </source>
+        <translation>&lt;b&gt;Map met collectd-databases wanneer de daemon gestopt is:&lt;/b&gt; </translation>
+    </message>
+</context>
+<context>
+    <name>ApplicationWindowPL</name>
+    <message>
+        <source>Zoom In</source>
+        <translation>Inzoomen</translation>
+    </message>
+    <message>
+        <source>Zoom Out</source>
+        <translation>Uitzoomen</translation>
+    </message>
+    <message>
+        <source>Now</source>
+        <translation>Nu</translation>
+    </message>
+    <message>
+        <source>&lt;---</source>
+        <translation>&lt;---</translation>
+    </message>
+    <message>
+        <source>---&gt;</source>
+        <translation>---&gt;</translation>
+    </message>
+    <message>
+        <source>1 hour</source>
+        <translation>1 uur</translation>
+    </message>
+    <message>
+        <source>4 hours</source>
+        <translation>4 uur</translation>
+    </message>
+    <message>
+        <source>8 hours</source>
+        <translation>8 uur</translation>
+    </message>
+    <message>
+        <source>12 hours</source>
+        <translation>12 uur</translation>
+    </message>
+    <message>
+        <source>24 hours</source>
+        <translation>24 uur</translation>
+    </message>
+    <message>
+        <source>3 days</source>
+        <translation>3 dagen</translation>
+    </message>
+    <message>
+        <source>1 week</source>
+        <translation>1 week</translation>
+    </message>
+    <message>
+        <source>4 weeks</source>
+        <translation>4 weken</translation>
+    </message>
+    <message>
+        <source>100 days</source>
+        <translation>100 dagen</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Hulp</translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation>Status</translation>
+    </message>
+    <message>
+        <source>Report</source>
+        <translation>Rapportage</translation>
+    </message>
+</context>
+<context>
+    <name>ColumnLayoutDialog</name>
+    <message>
+        <source>Show Details...</source>
+        <translation>Details tonen…</translation>
+    </message>
+</context>
+<context>
+    <name>GraphList</name>
+    <message>
+        <source>Welcome</source>
+        <translation>Welkom</translation>
+    </message>
+    <message>
+        <source>Empty page with undefined title</source>
+        <translation>Lege pagina zonder titel</translation>
+    </message>
+</context>
+<context>
+    <name>MessageAboutPL</name>
+    <message>
+        <source>version: </source>
+        <translation>versie: </translation>
+    </message>
+</context>
+<context>
+    <name>MessageErrorPL</name>
+    <message>
+        <source>Error</source>
+        <translation>Fout</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPL</name>
+    <message>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <source>Run collectd</source>
+        <translation>collectd uitvoeren</translation>
+    </message>
+    <message>
+        <source>Enable collectd on boot</source>
+        <translation>collectd inschakelen bij opstarten</translation>
+    </message>
+    <message>
+        <source>collectd folder while running, full pathname [/tmp/collectd/&lt;hostname&gt;]: </source>
+        <translation>collectd-map tijdens uitvoeren, volledige padnaam [/tmp/collectd/&lt;hostname&gt;]: </translation>
+    </message>
+    <message>
+        <source>collectd folder when stopped, full pathname [~/.local/share/collectd/&lt;hostname&gt;]: </source>
+        <translation>collectd-map wanneer gestopt, volledige padnaam [~/.local/share/collectd/&lt;hostname&gt;]: </translation>
+    </message>
+    <message>
+        <source>Update graphs interval [seconds]:</source>
+        <translation>Update-interval van grafieken [seconden]:</translation>
+    </message>
+    <message>
+        <source>Graph height in pixels:</source>
+        <translation>Hoogte van grafieken in pixels:</translation>
+    </message>
+    <message>
+        <source>Graphs: Title font size:</source>
+        <translation>Grafieken: lettergrootte van titel:</translation>
+    </message>
+    <message>
+        <source>Graphs: Axis font size:</source>
+        <translation>Grafieken: lettergrootte van as:</translation>
+    </message>
+    <message>
+        <source>Graphs: Unit font size:</source>
+        <translation>Grafieken: lettergrootte van eenheid</translation>
+    </message>
+    <message>
+        <source>Graphs: Legend font size:</source>
+        <translation>Grafieken: lettergrootte van legenda:</translation>
+    </message>
+    <message>
+        <source>Load new graph definitions from URL:</source>
+        <translation>Nieuwe grafiekdefinities laden van URL:</translation>
+    </message>
+    <message>
+        <source>Generate graph definitions using systemdatascope-makeconfig:</source>
+        <translation>Grafiekdefinities genereren met systemdatascope-makeconfig:</translation>
+    </message>
+    <message>
+        <source>collectd folder while running</source>
+        <translation>collectd-map tijdens uitvoeren</translation>
+    </message>
+    <message>
+        <source>Folder with the collectd databases while running. For example, /tmp/collectd/&lt;hostname&gt;</source>
+        <translation>Map met collectd-databases tijdens uitvoeren. Bijvoorbeeld: /tmp/collectd/&lt;hostname&gt;</translation>
+    </message>
+    <message>
+        <source>collectd folder while stopped</source>
+        <translation>collectd-map wanneer gestopt</translation>
+    </message>
+    <message>
+        <source>Folder with the collectd databases while the daemon is stopped. For example, ~/.local/share/collectd/&lt;hostname&gt;</source>
+        <translation>Map met collectd-databases wanneer de daemon gestopt is. Bijvoorbeeld: ~/.local/share/collectd/&lt;hostname&gt;</translation>
+    </message>
+    <message>
+        <source>Update graphs interval [seconds]</source>
+        <translation>Update-interval van grafieken [seconden]</translation>
+    </message>
+    <message>
+        <source>Graphs: Title font size</source>
+        <translation>Grafieken: lettergrootte van titel</translation>
+    </message>
+    <message>
+        <source>Graphs: Axis font size</source>
+        <translation>Grafieken: lettergrootte van as</translation>
+    </message>
+    <message>
+        <source>Graphs: Unit font size</source>
+        <translation>Grafieken: lettergrootte van eenheden</translation>
+    </message>
+    <message>
+        <source>Graphs: Legend font size</source>
+        <translation>Grafieken: lettergrootte van legenda</translation>
+    </message>
+    <message>
+        <source>Graph definitions can be generated by running systemdatascope-makeconfig automatically</source>
+        <translation>Grafiekdefinities kunnen gegenereerd worden door systemdatascope-makeconfig automatisch uit te voeren</translation>
+    </message>
+    <message>
+        <source>Generate definitions</source>
+        <translation>Definities genereren</translation>
+    </message>
+    <message>
+        <source>The graph definitions can be loaded from remote or local URL (use file:// for local URL). For that, type URL to graph definition in JSON format and press Load definitions button below. Note that acceping the new settings will not load the definitions</source>
+        <translation>De grafiekdefinities kunnen geladen worden van een externe of lokale URL (gebruik file:// voor een lokale URL). Voer daarvoor de URL naar de grafiekdefinitie in in JSON-formaat en tik hieronder op de knop ‘Definities laden’. Let op: de nieuwe instellingen aanvaarden zal de definities nog niet laden</translation>
+    </message>
+    <message>
+        <source>Interface configuration URL</source>
+        <translation>Interfaceconfiguratie-URL</translation>
+    </message>
+    <message>
+        <source>Load definitions</source>
+        <translation>Definities laden</translation>
+    </message>
+    <message>
+        <source>Report: height in pixels:</source>
+        <translation>Rapportage: hoogte in pixels:</translation>
+    </message>
+    <message>
+        <source>Graphs: height in pixels</source>
+        <translation>Grafieken: hoogte in pixels</translation>
+    </message>
+    <message>
+        <source>Report: height in pixels</source>
+        <translation>Rapportage: hoogte in pixels</translation>
+    </message>
+</context>
+<context>
+    <name>StatusPL</name>
+    <message>
+        <source>Status</source>
+        <translation>Status</translation>
+    </message>
+</context>
+<context>
+    <name>TimeControlPL</name>
+    <message>
+        <source>Time axis control</source>
+        <translation>Tijdsascontrole</translation>
+    </message>
+    <message>
+        <source>Zoom</source>
+        <translation>Zoom</translation>
+    </message>
+    <message>
+        <source>Shift</source>
+        <translation>Verschuiving</translation>
+    </message>
+    <message>
+        <source>Time window duration</source>
+        <translation>Tijdsvensterduur</translation>
+    </message>
+    <message>
+        <source>1 hour</source>
+        <translation>1 uur</translation>
+    </message>
+    <message>
+        <source>4 hours</source>
+        <translation>4 uur</translation>
+    </message>
+    <message>
+        <source>8 hours</source>
+        <translation>8 uur</translation>
+    </message>
+    <message>
+        <source>12 hours</source>
+        <translation>12 uur</translation>
+    </message>
+    <message>
+        <source>24 hours</source>
+        <translation>24 uur</translation>
+    </message>
+    <message>
+        <source>3 days</source>
+        <translation>3 dagen</translation>
+    </message>
+    <message>
+        <source>1 week</source>
+        <translation>1 week</translation>
+    </message>
+    <message>
+        <source>4 weeks</source>
+        <translation>4 weken</translation>
+    </message>
+    <message>
+        <source>100 days</source>
+        <translation>100 dagen</translation>
+    </message>
+    <message>
+        <source>1 year</source>
+        <translation>1 jaar</translation>
+    </message>
+    <message>
+        <source>Main graphs</source>
+        <translation>Hoofdgrafieken</translation>
+    </message>
+    <message>
+        <source>2 hours</source>
+        <translation>2 uur</translation>
+    </message>
+    <message>
+        <source>Cover graphs</source>
+        <translation>Hoesgrafieken</translation>
+    </message>
+</context>
+</TS>
